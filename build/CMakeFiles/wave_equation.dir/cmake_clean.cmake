f6f86f96cb28fa18072409765cbbea1431170bb7
saifekhaled <saifkhaledd1511@gmail.com>
2026-04-12 19:51:02 +0200

    first trial

---
+++ build/CMakeFiles/wave_equation.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/wave_equation.dir/src/wave_equation.cc.o"
+  "CMakeFiles/wave_equation.dir/src/wave_equation.cc.o.d"
+  "wave_equation"
+  "wave_equation.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/wave_equation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
